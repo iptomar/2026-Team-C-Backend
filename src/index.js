93c7b4e92c46e4d193817babbb02fe1def609705
Daniel Paulo <aluno25966@ipt.pt>
2026-05-01 23:07:21 +0100

    Add dotenv configuration to index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const authRouter = require('./routes/auth');
 const formsRouter = require('./routes/forms');
-
+require('dotenv').config();
 const app = express();
 
 app.use(cors({
