bfd0e3f960b015ffebcf8f8da57a7c2081b1465c
DanielFFPaulo <aluno25966@ipt.pt>
2026-05-15 21:45:48 +0100

    Trocar servico de email para o SendGrid
    Neste commit for alterado o sistema de envio de email do nodemailer para o sendgrid

--- src/email/transporter.js
+++ src/email/transporter.js
@@ -1,35 +1,22 @@
-const nodemailer = require("nodemailer");
-require('dotenv').config()
+const sgMail = require("@sendgrid/mail");
+require("dotenv").config();
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendEmail(to, subject, text) {
-  const transporter = nodemailer.createTransport({
-  service: "gmail",
-  host: 'smtp.gmail.com',
-  port: 587,
-  secure: false,
-  auth: {
-    user: "lusobites.contact@gmail.com",
-    pass: process.env.GOOGLE_APP_PASSWORD, // The 16-character App Password
-  },
-  });
-
-  // Configure the mailoptions object
   const mailOptions = {
-    from: 'lusobites.contact@gmail.com',
+    from: "lusobites.contact@gmail.com",
     to: to,
     subject: subject,
-    text: text
+    text: text,
   };
 
-  // Send the email
-  transporter.sendMail(mailOptions, function(error, info){
-    if (error) {
-      console.log('Error:', error);
-    } else {
-      console.log('Email sent: ', info.response);
-    }
-  });
+  try {
+    await sgMail.send(mailOptions);
+    console.log("Email sent successfully");
+  } catch (error) {
+    console.log("Error:", error);
   }
+}
 
-
-module.exports = {sendEmail};
+module.exports = { sendEmail };
